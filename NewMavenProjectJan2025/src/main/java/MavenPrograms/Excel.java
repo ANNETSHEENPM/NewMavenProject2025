b078bb6b00d46c975ea94301fce604800ed2310c
annet <annet@AnnetLibin>
2025-05-16 06:35:25 +0530

    new

---
+++ NewMavenProjectJan2025/src/main/java/MavenPrograms/Excel.java
@@ -0,0 +1,34 @@
+package MavenPrograms;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Excel {
+	XSSFSheet sh;	
+	public Excel() throws IOException {
+		FileInputStream f=new FileInputStream("C:\\Users\\annet\\OneDrive\\Documents\\Obsqura_Zone\\Files\\Excel.xlsx");
+		 XSSFWorkbook wb=new XSSFWorkbook(f);
+		 sh=wb.getSheet("Sheet1");
+	 }
+	public String readData(int i, int j) {
+		
+		XSSFRow r=sh.getRow(i);
+		Cell c=r.getCell(j);
+		int celltype=c.getCellType();
+		switch(celltype) {
+		case Cell.CELL_TYPE_NUMERIC:
+		{
+			double d=c.getNumericCellValue();
+			return String.valueOf(d);
+		}
+		case Cell.CELL_TYPE_STRING:
+		{
+			return c.getStringCellValue();
+		}
+		}	
+		return null;				
+	}
+}
